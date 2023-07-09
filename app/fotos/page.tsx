000d1dde4d43a19baf22ca071add263038688ffa
Marcel Weber <mrweber@gmx.ch>
2023-07-09 08:39:06 +0200

    Basic render of zenphoto albums

--- app/fotos/page.tsx
+++ app/fotos/page.tsx
@@ -1,10 +1,69 @@
+import AlbumContainer from "@/components/AlbumContainer";
 
-export default function Home() {
+interface Image{
+  path: string;
+  title: string;
+  date: number;
+  url_full: string;
+  url_sized: string;
+  url_thumb: string;
+  width: number;
+  height: number;
+  index: Number;
+}
+
+interface Next{
+  path: string;
+  title: string;
+  date: number;
+  date_updated: number;
+  image_size: number;
+  thumb_size: number;
+  url_thumb: string;
+}
+
+interface Albums{
+  path: string;
+  title: string;
+  date: number;
+  date_updated: number;
+  image_size: number;
+  thumb_size: number;
+  url_thumb: string;
+  images: Image[];
+  next: Next;
+}
+
+interface Album{
+  title: string;
+  desc: string;
+  path: string;
+  image_size: number;
+  thumb_size: number;
+  albums: Albums[];
+}
+
+interface Data{
+  album: Album;
+}
+
+async function getAlbums(){
+  const getAlbums: Response = await fetch("https://zenphoto.mrweber.ch/?json&pagination=off&depth=2")
+  return getAlbums.json()
+}
+
+
+export default async function Home() {
+
+  const data:Data = await getAlbums()
+
   return (
     <section>
-        <h1>Title</h1>
+        <h1>FOTOS</h1>
         <article>
-          content
+        {data.album.albums.map(album=>{
+            return <AlbumContainer album={album} />
+          })}
         </article>
     </section>
   )
