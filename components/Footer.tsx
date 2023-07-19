5272406b244b416d2b4d1902bc8a6b0d1a359d40
Marcel Weber <mrweber@gmx.ch>
2023-07-19 14:09:32 +0200

    Footer styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Link from "next/link"
+import s from "@/styles/Footer.module.css"
 
 export default function Footer(){
     return(
-        <footer>
+        <footer className={s.footer}>
             Alle Bilder enstammen <Link href="https://beta.dreamstudio.ai/" target="_blank">DreamStudio (Stable Diffusion SDXL Beta)</Link> Alle Texte entstammen <Link href="https://chat.openai.com/" target="_blank">ChatGPT-3</Link> mit Ausnahme von Adresse und Kontaktdaten. Diese sind aber ebenfalls fiktiv. Die Webseite ist ein Demo-Projekt von <Link href="https://mrweber.ch" target="_blank">mrweber.ch</Link>
         </footer>
     )
